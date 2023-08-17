452606891ed39096bb2f73e3b83041d09b4bd22e
dev-kudli <sudarshan97.kudli@gmail.com>
2023-08-17 15:41:42 -0400

    message functionality for new message

--- server/controller/messageController.js
+++ server/controller/messageController.js
@@ -1,14 +1,25 @@
 const messageModel = require("../model/messageModel");
 
+// new message
+const newMessage = async (request, response) => {
+  const newMessage = new messageModel(request.body);
+  try {
+    await newMessage.save();
+    // await messageModel.findByIdAndUpdate(request.body.conversationId, { message: request.body.text });
+    response.status(200).json("Message has been sent successfully");
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
+
 // recent messages between user A and B
 const getRecentMessages = async (request, response) => {
   try {
     const messages = await messageModel
       .find({
-        senderId: request.query.senderId,
-        receiverId: request.query.receiverId,
+        conversationId: request.query.conversationId,
       })
-      .sort({ createdAt: -1 })
+      .sort({ createdAt: 1 })
       .limit(100);
     response.status(200).json(messages);
   } catch (error) {
@@ -17,5 +28,6 @@ const getRecentMessages = async (request, response) => {
 };
 
 module.exports = {
-  getRecentMessages,
-};
+  newMessage,
+  getRecentMessages
+}
