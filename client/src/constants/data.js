beb716ce47e9ab09bf8c61fd71b673b253e19c30
dev-kudli <sudarshan97.kudli@gmail.com>
2023-09-15 13:27:15 -0400

    added custom google clientid

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -1,4 +1,4 @@
-export const clientId = '1051426514050-dv0o9fu9umand32ov9iqcamvuf9gttta.apps.googleusercontent.com'
+export const clientId = '165711366387-o1kesm68a8q72pqd5f8c2sac47h425aj.apps.googleusercontent.com'
 
 export const iconPDF = 'https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/27_Pdf_File_Type_Adobe_logo_logos-512.png';
 export const defaultProfilePicture = 'https://static.straitstimes.com.sg/s3fs-public/articles/2020/12/01/af_moneyheist_011220.jpg';
